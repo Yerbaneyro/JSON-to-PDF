6257d528867d65b972d1222749ee59b03cb6ef1a
Yerbaneyro <94953520+Yerbaneyro@users.noreply.github.com>
2022-01-03 02:37:26 +0100

    A bit of polishing
    Added Test script to package

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const fs = require('fs'); 
+const port = 3000; 
 const PDFDocument = require('pdfkit');
-const doc = new PDFDocument;
 
 app.use(express.json())
 
